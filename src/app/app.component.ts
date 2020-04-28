ea92cbbf9aa0cd222d6f90fdefe4f2fe42eb5073
piupaulgit <piupaul99design@gmail.com>
2020-04-29 01:00:50 +0530

    table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { employeeData, columns } from '../assets/data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'nestedNgxDataTable';
+export class AppComponent implements OnInit {
+  rows: any = employeeData;
+  columns: any = columns;
+
+  ngOnInit() {}
+
+  isTreeColumn(columnLabel) {
+    if (columnLabel === 'name') {
+      return true;
+    }
+  }
+
+  onTreeAction(event: any) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
